5621ef36248e554125ffd2325ddba25b3685006c
dkotula <dkotula.wieliczka@gmail.com>
2022-12-06 16:27:21 +0100

    Barriers pushing fix

--- js/board.js
+++ js/board.js
@@ -981,7 +981,7 @@ class Board {
 
     addBarrier(number) {
         for (let i = 0; i < this.options.board.width; i++) {
-            if (i < this.options.board.width - 2) {
+            if (number < this.options.board.width - 2) {
                 this.fields[i][number + 1].setLife(this.fields[i][number + 1].getColor(), this.fields[i][number].getLife() + this.fields[i][number + 1].getLife())
             }
             this.fields[i][number].makeBlock();
